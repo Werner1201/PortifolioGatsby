467276cc68defd6f5bf474e6ab215d80dac81381
Werner1201 <werner.romling@unigranrio.br>
2020-04-18 10:35:52 -0300

    Modificando o template inicial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Werner's Portifolio`,
+    description: `Um Lugar onde posso mostrar o que sei, faço e penso. Seja profissional, casual ou artístico`,
+    author: `@WRomling`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,21 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
