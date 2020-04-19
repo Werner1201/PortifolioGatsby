a3b0e637f74afd2b53ae896bfe75b7a114588d23
Werner1201 <werner.romling@unigranrio.br>
2020-04-19 18:55:36 -0300

    Adicionei a API do source GraphQl

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,71 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `GitHub`,
+        fieldName: `github`,
+        url: `https://api.github.com/graphql`, //highlight-line
+        headers: {
+          Authorization: `Bearer `,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-github-api`,
+      options: {
+        // url: API URL to use. Defaults to  https://api.github.com/graphql
+        token: `76d89536b396a78d20be7ae11bcf4faf9d10e44e`,
+        graphQLQuery: `
+        query ($author: String = "", $userFirst: Int = 0, $searchFirst: Int = 0, $q: String = "") {
+          user(login: $author) {
+            repositories(first: $userFirst, orderBy: {field: STARGAZERS, direction: DESC}) {
+              edges {
+                node {
+                  name
+                  description
+                  url
+                  stargazers {
+                    totalCount
+                  }
+                  readme: object(expression:"master:README.md"){
+                    ... on Blob{
+                      text
+                    }
+                  }
+                }
+              }
+            }
+          }
+          search(query: $q, type: ISSUE, first: $searchFirst) {
+            edges {
+              node {
+                ... on PullRequest {
+                  title
+                  merged
+                  url
+                  state
+                  repository {
+                    stargazers {
+                      totalCount
+                    }
+                    repoUrl: url
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }`,
+        variables: {
+          userFirst: 0,
+          searchFirst: 0,
+          q: "user:Werner1201",
+          author: "Werner1201",
+        },
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
